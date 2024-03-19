9c904ffe2738b1f8554952778c069d76ded90490
faisalakb <faisalakb786@gmail.com>
2024-03-19 13:51:18 +0500

    Add react-redux setup

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+import House from './features/House'
+
+function App() {
+  return (
+    <div className="App">
+    <h1>hello pakistan</h1>
+    <House/>
+    </div>
+  );
+}
+
+export default App;
