2774e5aaa6d9b8716db913fc3ea1b3d299667f82
Ochiengsteven <stevenochieng6305@gmail.com>
2024-03-23 12:21:18 +0300

    ADD ICON

--- src/features/house/components/Houses.js
+++ src/features/house/components/Houses.js
@@ -2,6 +2,7 @@ import React, { useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { Link } from 'react-router-dom';
 import { fetchAllHouses } from '../getHousesSlice';
+import Dots from '../../../assets/dots.png';
 
 const HouseList = () => {
   const dispatch = useDispatch();
@@ -31,12 +32,13 @@ const HouseList = () => {
       <div className="flex scroll-smooth">
         {houses.map((house) => (
           <Link key={house.id} to={`/house/${house.id}`} className="house-card-link shrink-0">
-            <div className="rounded-[8px] m-2 overflow-hidden w-[75vw] h-[55vh] shadow-lg">
+            <div className="rounded-[8px] m-2 overflow-hidden w-[75vw] h-[55vh] shadow-lg relative">
+              <img className="absolute right-2 bottom-8 w-8 h-8" src={Dots} alt="dots" />
               <div className="photo">
                 <img className="h-[45vh] w-full" src={house.photo} alt={house.title} />
               </div>
               <div className="house-details p-2 font-semibold">
-                <h2>{house.title}</h2>
+                <h2 className="pt-6 px-3">{house.title}</h2>
               </div>
             </div>
           </Link>
